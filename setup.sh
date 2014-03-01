9b3354d4609418fe3c7aac390603660976a5b244
Jacqueline Leykam <jleykam@gmail.com>
2014-03-02 10:55:03 +1100

    Make the symlinks work properly.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+INSTALLDIR=$HOME/.dotfiles
 
 # Terminator config
-mkdir -p ~/.config/terminator
-ln -sf terminator/config ~/.config/terminator/config
+mkdir -p $HOME/.config/terminator
+ln -sf $INSTALLDIR/terminator/config $HOME/.config/terminator/config
 
 # Vim
-ln -sf vimrc ~/.vimrc
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -sf $INSTALLDIR/vimrc $HOME/.vimrc
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 # TODO: Maybe also run BundleInstall and compile YCM?
 
 # Zsh
-ln -sf zshrc ~/.zshrc
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+ln -sf $INSTALLDIR/zshrc ~/.zshrc
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 # For some reason zsh freaks out and doesn't detect this if I make it a link.
 # TODO: Work out why.
-mkdir -p ~/.oh-my-zsh/custom/themes
-cp jqln.zsh-theme ~/.oh-my-zsh/custom/themes/jqln.zsh-theme
+mkdir -p $HOME/.oh-my-zsh/custom/themes
+ln -sf $INSTALLDIR/jqln.zsh-theme $HOME/.oh-my-zsh/custom/themes/jqln.zsh-theme
 
