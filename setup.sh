abb259e6c988229a8fa61d8d66f8a9fd1947eec7
Jacqueline Leykam <jleykam@gmail.com>
2017-01-23 20:38:05 +1100

    Add .ideavimrc to the setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ INSTALLDIR=$HOME/.dotfiles
 
 # Vim
 ln -sf $INSTALLDIR/vimrc $HOME/.vimrc
+ln -sf $INSTALLDIR/vimrc $HOME/.ideavimrc
 
 # Zsh
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
